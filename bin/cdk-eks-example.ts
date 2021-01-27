1614d2f36c92dbb9ae9e209121cdbf3cddcb0dd9
roy <roy4885@gmail.com>
2021-01-27 22:56:52 +0900

    update cdk-eks-example.ts

--- bin/cdk-eks-example.ts
+++ bin/cdk-eks-example.ts
@@ -1,16 +1,34 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { VpcStack } from '../lib/vpc-stack';
-import { EksClusterStack } from '../lib/eks-cluster-stack';
-import { LoadBalancerControllerStack } from '../lib/lb-controller';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { VpcStack } from "../lib/vpc-stack";
+import { EksClusterStack } from "../lib/eks-cluster-stack";
+import { LoadBalancerControllerStack } from "../lib/lb-controller";
+import { ExternalDnsStack } from "../lib/external-dns"
 
 const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = process.env.CDK_DEFAULT_REGION;
-const deployEnv = {account: account, region: region};
+const deployEnv = { account: account, region: region };
+const domainName = 'example.com';
 
-const vpcStack = new VpcStack(app, `VpcStack-${deployEnv.region}`, {env: deployEnv});
-const clusterStack = new EksClusterStack(app, `EksClusterStack`, {env: deployEnv, vpc: vpcStack.vpc});
-new LoadBalancerControllerStack(app, 'LoadBalancerControllerStack', {env: deployEnv, url: clusterStack.clusterOpenIdConnectIssuerUrl, cluster: clusterStack.cluster});
+const vpcStack = new VpcStack(app, `VpcStack-${deployEnv.region}`, {
+  env: deployEnv,
+});
+
+const clusterStack = new EksClusterStack(app, `EksClusterStack`, {
+  env: deployEnv,
+  vpc: vpcStack.vpc,
+});
+
+new LoadBalancerControllerStack(app, "LoadBalancerControllerStack", {
+  env: deployEnv,
+  cluster: clusterStack.cluster,
+});
+
+new ExternalDnsStack(app, "ExternalDnsStack", {
+  env: deployEnv,
+  cluster: clusterStack.cluster,
+  domainName: domainName
+})
 
