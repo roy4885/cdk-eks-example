eaec38d0c89e4d3ae9691ca86547e9603d3c2231
roy <roy4885@gmail.com>
2021-01-27 22:54:38 +0900

    add aws load balancer controller

--- lib/lb-controller.ts
+++ lib/lb-controller.ts
@@ -1,71 +1,53 @@
-import * as iam from '@aws-cdk/aws-iam'
-import * as cdk from '@aws-cdk/core'
-import * as eks from '@aws-cdk/aws-eks'
-import { CfnOutput, CfnJson } from '@aws-cdk/core';
-import * as iampolicy from './iam_policy.json'
+import * as iam from "@aws-cdk/aws-iam";
+import * as cdk from "@aws-cdk/core";
+import * as eks from "@aws-cdk/aws-eks";
+import * as iampolicy from "./iam_policy.json";
+import { readYamlFromDir } from "../utils/read-file";
 
 export interface LoadBalancerControllerStackProps extends cdk.StackProps {
-    url: string,
-    cluster: eks.ICluster
+  cluster: eks.Cluster;
 }
 
 export class LoadBalancerControllerStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props: LoadBalancerControllerStackProps) {
-        super(scope, id, props);
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: LoadBalancerControllerStackProps
+  ) {
+    super(scope, id, props);
 
-        // new CfnOutput(this, 'openidprovider', { value: props.url});
-        // const oidcprovider = new iam.OpenIdConnectProvider(this, 'OpenIdConnectProvider', {
-        //     url: props.url,
-        //     clientIds: ['sts.amazonaws.com']
-        // });
-        // const openIdConnectProviderArn = oidcprovider.openIdConnectProviderArn;
-        // const stringEquals = new CfnJson(this, 'OidcConditionJson', {
-        //     value: {
-        //       [`${oidcprovider.openIdConnectProviderIssuer}:aud`]: 'sts.amazonaws.com',
-        //       [`${oidcprovider.openIdConnectProviderIssuer}:sub`]: 'system:serviceaccount:kube-system:aws-load-balancer-controller',
-        //     },
-        //   });
+    const lbControllerSA = new eks.ServiceAccount(
+      this,
+      "LoadBalancerControllerSA",
+      {
+        cluster: props.cluster,
+        name: "aws-load-balancer-controller",
+        namespace: "kube-system",
+      }
+    );
 
-        // const lbRole = new iam.Role(this, 'EKSLoadBalancerControllerRole', {
-        //     assumedBy: new iam.WebIdentityPrincipal(
-        //         openIdConnectProviderArn,
-        //         {StringEquals: stringEquals}),
-        //     roleName: 'EKSLoadBalancerControllerRole',
-        //     });
-        
+    new iam.Policy(this, "LoadBalancerControllerPolicy", {
+      policyName: "AWSLoadBalancerControllerIAMPolicy",
+      document: iam.PolicyDocument.fromJson(iampolicy),
+      roles: [lbControllerSA.role],
+    });
 
-        // new iam.Policy(this, 'LoadBalancerControllerPolicy', {
-        //     policyName: 'AWSLoadBalancerControllerIAMPolicy',
-        //     document: iam.PolicyDocument.fromJson(iampolicy),
-        //     roles: [lbRole]
-        // })
-        
-        const lbControllerSA = new eks.ServiceAccount(this, 'LoadBalancerControllerSA', {
-            cluster: props.cluster,
-            name: 'aws-load-balancer-controller',
-            namespace: 'kube-system'
-        })
+    const lbControllerFolder = "./load-balancer-controller/";
+    readYamlFromDir(lbControllerFolder, props.cluster);
 
-        new iam.Policy(this, 'LoadBalancerControllerPolicy', {
-            policyName: 'AWSLoadBalancerControllerIAMPolicy',
-            document: iam.PolicyDocument.fromJson(iampolicy),
-            roles: [lbControllerSA.role]
-        })
-
-        new eks.HelmChart(this, `LoadBalancerControllerChart`, {
-            cluster: props.cluster,
-            repository: 'https://aws.github.io/eks-charts',
-            chart: 'aws-load-balancer-controller',
-            release: 'aws-load-balancer-controller',
-            wait: true,
-            namespace: 'kube-system',
-            values: {
-              'serviceAccount.create': false,
-              'serviceAccount.name': 'aws-load-balancer-controller'
-            }
-          });
-            
+    new eks.HelmChart(this, `LoadBalancerControllerChart`, {
+      cluster: props.cluster,
+      repository: "https://aws.github.io/eks-charts",
+      chart: "aws-load-balancer-controller",
+      release: "aws-load-balancer-controller",
+      namespace: "kube-system",
+      values: {
+        serviceAccount: {
+          create: false,
+          name: "aws-load-balancer-controller",
+        },
+        clusterName: props.cluster.clusterName,
+      },
+    });
   }
-
-
 }
